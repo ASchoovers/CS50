38500c023b80994f704e5965461a98b8e21e1f97
ASchoovers <35370315+ASchoovers@users.noreply.github.com>
2020-07-05 09:56:00 +0100

    Create cash.c
    A change calculator written in C

---
+++ pset1/cash.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <math.h>
+
+int main(void)
+{
+
+    float a;
+    int cents;
+    int change = 0;
+
+    // Get user input
+    do
+    {
+        a = get_float("Change owed: ");
+        // Round to amount of cents
+        cents = round(a * 100);
+
+    }
+    while (a <= 0);
+
+    // Check for quarters
+    while (cents >= 25)
+    {
+        change = change + 1;
+        cents = cents - 25;
+    }
+    //Check for Dimes
+    while (cents >= 10)
+    {
+        change = change + 1;
+        cents = cents - 10;
+    }
+    // Check for Nickels
+    while (cents >= 5)
+    {
+        change = change + 1;
+        cents = cents - 5;
+    }
+    // Check for Pennies
+    while (cents >= 1)
+    {
+        change = change + 1;
+        cents = cents - 1;
+    }
+    // Print total coins
+    printf("Total Coins: %i\n", change);
+
+
+
+
+}
